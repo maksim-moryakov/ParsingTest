252f4dc6d8f855fd63613b9fbd77de76e75e3e5e
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-03-24 12:49:28 +0300

    Переработана функция парсинга

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-import requests
+# import requests
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-url = 'https://ya.ru/'
+# получаем исходный код страницы
+inner_html_code = str(urlopen('https://thecode.media/parsing/').read(),'utf-8')
 
-headers = {
-    'Accept': '*/*',
-    'User-Agent': 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.5.708 Yowser/2.5 Safari/537.36'
-}
+# отправляем исходный код страницы на обработку в библиотеку
+inner_soup = BeautifulSoup(inner_html_code, "html.parser")
 
-req = requests.get(url, headers=headers)
-src = req.text
-print(src)
+# оставляем только блок с содержимым статьи
+inner_soup_1 = inner_soup.find('div', {"class": 'article-content'})
+
+def delete_div(code, tag, arg):
+    for div in code.find_all(tag, arg):
+        div.decompose()
+
+# удаляем титры
+delete_div(inner_soup_1, 'div', {'class': 'wp-block-lazyblock-titry'})
+
+# выводим содержимое страницы
+print(inner_soup.get_text())
